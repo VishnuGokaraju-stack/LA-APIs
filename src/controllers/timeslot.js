cd6285c2494b7233eaf8af476a85f6f29e9cb347
vishnugokaraju <vishnugokaraju@gmail.com>
2020-12-15 18:33:34 +0530

    timeslot get method

--- src/controllers/timeslot.js
+++ src/controllers/timeslot.js
@@ -3,11 +3,25 @@ const store = require('../models/store');
 
 exports.getTimeslot = async (req, res) => {
   try {
+    if (typeof req.query.storeId !== 'undefined' && req.query.storeId !== '') {
+      let timeslotData = await store.find({ _id: req.query.storeId });
+      if (timeslotData) {
+        return res.json({
+          error: null,
+          data: { timeSlots: timeslotData[0].timeSlots },
+        });
+      } else {
+        return res.status(400).json({
+          error: true,
+          message: 'Store not exist',
+        });
+      }
+    }
     let timeslotData = await company.find({ _id: req.user.companyId });
     if (timeslotData) {
       return res.json({
         error: null,
-        data: timeslotData,
+        data: { timeSlots: timeslotData[0].timeSlots },
       });
     } else {
       return res.status(400).json({
@@ -31,22 +45,42 @@ exports.updateTimeslot = async (req, res) => {
         message: 'Not authorized to access !',
       });
     }
-    let updateCompany = await company.findByIdAndUpdate(
-      { _id: req.user.companyId },
-      { $set: { timeSlots: req.body.timeSlots } },
-      { new: true, useFindAndModify: false }
-    );
-    if (updateCompany) {
-      res.status(200).json({
-        error: null,
-        message: 'Timeslot updated successfully',
-        data: updateCompany,
-      });
+    if (typeof req.query.storeId !== 'undefined' && req.query.storeId !== '') {
+      let updateStore = await store.findOneAndUpdate(
+        { _id: req.query.storeId },
+        { $set: { timeSlots: req.body.timeSlots } },
+        { new: true, useFindAndModify: false }
+      );
+      if (updateStore) {
+        res.status(200).json({
+          error: null,
+          message: 'Timeslot for the store updated successfully',
+          data: { timeSlots: updateStore.timeSlots },
+        });
+      } else {
+        return res.status(400).json({
+          error: true,
+          message: 'Timeslot not updated. Please try again',
+        });
+      }
     } else {
-      return res.status(400).json({
-        error: true,
-        message: 'Timeslot not updated. Please try again',
-      });
+      let updateCompany = await company.findByIdAndUpdate(
+        { _id: req.user.companyId },
+        { $set: { timeSlots: req.body.timeSlots } },
+        { new: true, useFindAndModify: false }
+      );
+      if (updateCompany) {
+        res.status(200).json({
+          error: null,
+          message: 'Timeslot updated successfully',
+          data: { timeSlots: updateCompany.timeSlots },
+        });
+      } else {
+        return res.status(400).json({
+          error: true,
+          message: 'Timeslot not updated. Please try again',
+        });
+      }
     }
   } catch (error) {
     return res.status(500).json({
